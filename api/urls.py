afeac0bf535f3497e32ca00d25c26c2032aacd1f
Charles Walton <pythontutorials36@gmail.com>
2025-02-12 23:16:01 +0100

    Adding Urls to Create and Delete Authors

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,14 @@
-from .views import main
+from .views import main, authors, get_author, create_author, delete_author
 from django.urls import path
 
 
 urlpatterns = [
-    path(' ', main,  name="home")
+    path(' ',  main,  name="home-view"),
+    path('authors/', authors, name='all-authors'),
+    path('authors/<int:id>', get_author, name='author'),
+    path('authors/create/', create_author, name='create-author'),
+    path('authors/delete/<int : id>/', delete_author, name='delete-author')
+
+
+
 ]
